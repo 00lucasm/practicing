6a610d97e26c6634372b329e23e519ae4fb3561e
Lucas Melo <34749742+Melo-Lucas@users.noreply.github.com>
2021-12-13 16:52:40 -0300

    Melhor versão para submeter ao URI

--- uri1000/Main.java
+++ uri1000/Main.java
@@ -1,8 +1,7 @@
-//import java.io.IOException;
-package uri1000;
+import java.io.IOException;
 
 public class Main{
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
         System.out.println("Hello World!");
     }
 }
