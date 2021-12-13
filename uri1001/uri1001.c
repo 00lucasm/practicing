c08e9af1f2891f781a7611de50193fd40b048524
Melo-Lucas <lucs.r06@gmail.com>
2021-12-13 17:34:55 -0300

    Inicia problema uri1001

---
+++ uri1001/uri1001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int A, B, X;
+
+    scanf("%i", &A);
+    scanf("%i", &B);
+
+    X = A + B;
+    printf("X = %i\n", X);
+
+    return 0;
+}
---
+++ uri1001/uri1001.php
@@ -0,0 +1,8 @@
+<?php
+    $A = $B = $X = 0;
+    $A = readline();
+    $B = readline();
+
+    $X = $A+$B;
+    print("X = $X\n");
+?>
