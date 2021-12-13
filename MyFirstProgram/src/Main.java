c7d9bb851f4962f2b094d97f2aa824a9e1072739
Melo-Lucas <lucs.r06@gmail.com>
2021-12-13 20:15:27 -0300

    Estudos específicos de Java

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,16 @@
+package MyFirstProgram.src;
+
+public class Main {
+    public static void main(String[] args){
+        String x = "water";
+        String y = "Kool-Aid";
+        String temp;
+
+        temp = x;
+        x = y;
+        y = temp;
+
+        System.out.println("x: "+x);
+        System.out.println("y: "+y);
+    }
+}
