f4b799f62a89d33d0d9a0ff6e0e6939326390c43
Melo-Lucas <lucs.r06@gmail.com>
2021-12-13 16:38:42 -0300

    Add package uri1000

--- uri1000/Main.java
+++ uri1000/Main.java
@@ -1,7 +1,8 @@
-import java.io.IOException;
+//import java.io.IOException;
+package uri1000;
 
 public class Main{
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         System.out.println("Hello World!");
     }
 }
