deca8458eba8ff53271447765750219ec7daa18f
Melo-Lucas <lucs.r06@gmail.com>
2021-12-14 16:57:31 -0300

    Soluciona desafio 1001

--- uri1001/Main.java
+++ uri1001/Main.java
@@ -1,11 +1,20 @@
 package uri1001;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        int A, B, X;
+        
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int X = A + B;
+
+        System.out.println("X = " + X);
+
     }
 
 }
