ff046c9de2b25c5e65a8ff94393df4a5a6a91def
Melo-Lucas <lucs.r06@gmail.com>
2021-12-13 21:59:36 -0300

    Leitura de valores da STDIN

---
+++ uri1001/Main.java
@@ -0,0 +1,13 @@
+package uri1001;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        int A, B, X;
+        Scanner scanner = new Scanner(System.in);
+        A = Integer.parseInt(scanner());
+    }
+
+}
