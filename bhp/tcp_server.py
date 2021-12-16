4ec38fb74a4f45ed6b624e4f016fe6999964befe
Melo-Lucas <lucs.r06@gmail.com>
2021-12-16 17:30:40 -0300

    teste

--- bhp/tcp_server.py
+++ bhp/tcp_server.py
@@ -21,7 +21,7 @@ def handle_client(client_socket):
     with client_socket as sock:
         request = sock.recv(1024)
         print(f'[*] Received: {request.decode("utf-8")}')
-        sock.send(b'ACK')
+        sock.send(b'token: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')
 
 if __name__ == '__main__':
     main()
