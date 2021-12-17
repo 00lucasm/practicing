679dadd5920c050a49dc121a6664da9e3b57a70c
Lucas Melo <34749742+Melo-Lucas@users.noreply.github.com>
2021-12-16 21:56:44 -0300

    Deleting directory

--- uri1000/Main.java
+++
@@ -1,8 +0,0 @@
-package uri1000;
-import java.io.IOException;
-
-public class Main{
-    public static void main(String[] args) throws IOException{
-        System.out.println("Hello World!");
-    }
-}
