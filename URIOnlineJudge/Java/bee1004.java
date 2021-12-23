a71e2046f4d66d16a0d5d4f8058d75aa1006e14a
Melo-Lucas <lucs.r06@gmail.com>
2021-12-23 18:28:43 -0300

    Solução em Java do desafio 1004

---
+++ URIOnlineJudge/Java/bee1004.java
@@ -0,0 +1,18 @@
+package URIOnlineJudge.Java;
+
+import java.util.Scanner;
+
+public class bee1004 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int X = A * B;
+
+        System.out.println("PROD = " + X);
+
+    }
+}
