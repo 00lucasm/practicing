d6445429e814ad08c4fbdb6aa8c6230645374d2d
Melo-Lucas <lucs.r06@gmail.com>
2021-12-23 18:29:06 -0300

    Solução em Java do desafio 1003

---
+++ URIOnlineJudge/Java/uri1003.java
@@ -0,0 +1,19 @@
+package URIOnlineJudge.Java;
+
+import java.util.Scanner;
+
+public class uri1003 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        scanner.close();
+
+        int X = A * B;
+
+        System.out.println("SOMA = " + X);
+
+    }
+    
+}
