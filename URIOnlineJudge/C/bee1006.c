7b9d10805d1ca7d74417367bcfc7c4dc907bc793
Melo-Lucas <lucs.r06@gmail.com>
2022-02-07 21:42:36 -0300

    Solução do desafio bee1006

---
+++ URIOnlineJudge/C/bee1006.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  double a, b, c, media;
+
+  scanf("%lf\n%lf\n%lf", &a, &b, &c);
+
+  media = (2*a + 3*b + 5*c)/10;
+
+  printf("MEDIA = %.1lf\n", media);
+
+  return 0;
+}
+
