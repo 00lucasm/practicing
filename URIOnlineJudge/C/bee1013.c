c89ed3478544d014e929c2fc7d26592a94a152be
Melo-Lucas <lucs.r06@gmail.com>
2022-02-09 17:19:46 -0300

    solucoes dos desafios 1013 e 1015

---
+++ URIOnlineJudge/C/bee1013.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int a, b, c, maiorAB;
+
+  scanf("%d %d %d", &a, &b, &c);
+
+  maiorAB = (a+b + abs(a-b))/2;
+
+  printf("%d eh o maior\n", (maiorAB + c + abs(maiorAB - c))/2);
+
+  return 0;
+}
+
