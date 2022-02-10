8c91466084da0b81ebeb278e4dde2a7f3b1fc9e1
Melo-Lucas <lucs.r06@gmail.com>
2022-02-10 17:36:32 -0300

    teste da solução - não oficial

--- URIOnlineJudge/Cpp/bee1061.cpp
+++ URIOnlineJudge/Cpp/bee1061.cpp
@@ -1,24 +1,8 @@
 #include <iostream>
-#include <cstring>
-#include <string>
 
 using namespace std;
 
-int main()
-{
-  string line1, line2, line3, line4;
-
-  getline(cin, line1, '\n');
-  getline(cin, line2, '\n');
-  getline(cin, line3, '\n');
-  getline(cin, line4, '\n');
-
-  // extrair os chars de dentro das strings
-  // converter o char para int
-  // converter tudo para segundos
-  // calcular diferença
-  // exibir na saída padrão
-  // fim :)
+int main(){
 
   return 0;
 }
