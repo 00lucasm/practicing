98d806c8d61c34c575f762971c3601f018ec8325
Melo-Lucas <lucs.r06@gmail.com>
2022-02-17 18:23:15 -0300

    lógica errada

--- URIOnlineJudge/C/bee1042.c
+++ URIOnlineJudge/C/bee1042.c
@@ -7,10 +7,5 @@ int main(){
   scanf("%d %d %d", &a, &b, &c);
   printf("%d\n%d\n%d\n", a, b, c);
 
-  if((a>b) && (b>c))
-    printf("%d\n%d\n%d\n", a, b, c);
-  else if((a>c) && (c>b))
-
-
   return 0;
 }
