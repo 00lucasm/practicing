3da9868e543cc7c94201ce0fb709a3ba8c5b0c13
Melo-Lucas <lucs.r06@gmail.com>
2022-02-17 21:15:02 -0300

    soluciona desafio 1042

--- URIOnlineJudge/C/bee1042.c
+++ URIOnlineJudge/C/bee1042.c
@@ -2,10 +2,45 @@
 
 int main(){
 
-  int a, b, c, cmp;
+  int x, y, z, menor, medio, maior;
 
-  scanf("%d %d %d", &a, &b, &c);
-  printf("%d\n%d\n%d\n", a, b, c);
+  scanf("%d %d %d", &x, &y, &z);
+
+	if(x>y && x>z){
+		maior = x;
+		if(y>z){
+			medio = y;
+			menor = z;
+		}else{
+			medio = z;
+			menor = y;
+		}
+	}else{
+		if(y>x && y>z){
+			maior = y;
+			if(x>z){
+				medio = x;
+				menor = z;
+			}else{
+				medio = z;
+				menor = x;	
+			}
+		}else{
+			if(z>x && z>y){
+				maior = z;
+				if(x>y){
+					medio = x;
+					menor = y;
+				}else{
+					medio = y;
+					menor = x;
+				}
+			}
+		}
+	}
+
+    printf("%d\n%d\n%d\n", menor, medio, maior);
+    printf("\n%d\n%d\n%d\n", x, y, z);
 
   return 0;
 }
