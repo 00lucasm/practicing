d85946e8074c203ab7254dfa196e145897b11f86
Melo-Lucas <lucs.r06@gmail.com>
2022-02-25 15:20:35 -0300

    Solução desafio final

--- URIOnlineJudge/C/bee1043.c
+++ URIOnlineJudge/C/bee1043.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
+    float a, b, c;
+
+    scanf("%f %f %f", &a, &b, &c);
+
+    if((a <= (b+c)) || (b <= (a+c)) || (c <= (a+b)))
+        printf("Perimetro = %f\n", a+b+c);
+    else
+        printf("Area = %f\n", (c*(a+b))/2);
 
     return 0;
 }
